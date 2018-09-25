d96cf0180f1993934e28110251eed455d613bbe2
Vitor Barros <vba@cesar.org.br>
2018-09-25 11:12:40 -0300

    samples: knot: Add function to register new I/O
    This patch adds a function to allow KNoT based applications to register
    a new sensor or actuator.
    
    Signed-off-by: Vitor Barros <vba@cesar.org.br>

--- core/src/kaio.c
+++ core/src/kaio.c
@@ -5,6 +5,7 @@
  */
 
 #include <zephyr.h>
+#include <string.h>
 
 #include "knot_protocol.h"
 #include "kaio.h"
@@ -19,7 +20,7 @@ static struct aio {
        u8_t                    value_type;
        u8_t                    unit;
        uint16_t                type_id;
-       const char              *name;
+       const char              *name; /* TODO */
 
        /* Data values */
        bool                    new_value;
@@ -32,4 +33,43 @@ static struct aio {
 
        /* Time values */
        u32_t                   last_timeout;
+
+       kaio_callback_t		read_cb;
+       kaio_callback_t		write_cb;
 } aio[KNOT_THING_DATA_MAX];
+
+
+s8_t kaio_register(u8_t id, const char *name,
+		   u16_t type_id, u8_t value_type, u8_t unit,
+		   kaio_callback_t read_cb, kaio_callback_t write_cb)
+{
+	struct aio *io;
+
+	/* Assigned already? */
+	if (aio[id].id != -1)
+		return -EACCES;
+
+	/* Basic field validation */
+	if (knot_schema_is_valid(type_id, value_type, unit) != 0 || !name)
+		return -EINVAL;
+
+	memset(&aio[id], 0, sizeof(aio[id]));
+	io = &aio[id];
+
+	io->id = id;
+	io->name = name;
+	io->type_id = type_id;
+	io->unit = unit;
+	io->value_type = value_type;
+	io->new_value = false;
+
+	/* Set default config */
+	io->config.event_flags = KNOT_EVT_FLAG_TIME;
+	io->config.time_sec = 30;
+
+	io->read_cb = read_cb;
+	io->write_cb = write_cb;
+
+	return 0;
+
+}
--- core/src/kaio.h
+++ core/src/kaio.h
@@ -3,3 +3,9 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+
+typedef int (*kaio_callback_t) (u8_t id);
+
+s8_t kaio_register(u8_t id, const char *name,
+		   u16_t type_id, u8_t value_type, u8_t unit,
+		   kaio_callback_t read_cb, kaio_callback_t write_cb);
