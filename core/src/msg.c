4d7d152c47e2238a141071f3ddee5b6b73202d70
Claudio Takahasi <ckt@cesar.org.br>
2018-09-26 14:19:03 -0300

    samples: knot: Use knot_msg as default argument
    When creating KNOT PDUs, helpers should always use knot_msg as
    argument in order to provide a clean API.
    
    Signed-off-by: Claudio Takahasi <ckt@cesar.org.br>

--- core/src/msg.c
+++ core/src/msg.c
@@ -9,6 +9,8 @@
 
 #include "knot_protocol.h"
 
+#include "msg.h"
+
 size_t msg_create_auth(knot_msg *msg, const char *uuid, const char *token)
 {
 	strncpy(msg->auth.uuid, uuid, KNOT_PROTOCOL_UUID_LEN);
@@ -20,19 +22,21 @@ size_t msg_create_auth(knot_msg *msg, const char *uuid, const char *token)
 	return (sizeof(msg->hdr) + msg->hdr.payload_len);
 }
 
-size_t msg_create_schema(knot_msg_schema *msg, u8_t id,
+size_t msg_create_schema(knot_msg *msg, u8_t id,
 		       const knot_schema *schema, bool end)
 {
 	msg->hdr.type = (end ? KNOT_MSG_SCHEMA_END : KNOT_MSG_SCHEMA);
-	msg->sensor_id = id;
+	msg->schema.sensor_id = id;
 
-	msg->values.value_type = schema->value_type;
-	msg->values.unit = schema->unit;
+	msg->schema.values.value_type = schema->value_type;
+	msg->schema.values.unit = schema->unit;
 	/* TODO: missing endianess */
-	msg->values.type_id = schema->type_id;
-	strncpy(msg->values.name, schema->name, KNOT_PROTOCOL_DATA_NAME_LEN);
+	msg->schema.values.type_id = schema->type_id;
+	strncpy(msg->schema.values.name, schema->name,
+		KNOT_PROTOCOL_DATA_NAME_LEN);
 
-	msg->hdr.payload_len = sizeof(msg->values) + sizeof(msg->sensor_id);
+	msg->hdr.payload_len = sizeof(msg->schema.values) +
+				sizeof(msg->schema.sensor_id);
 
 	return (sizeof(msg->hdr) + msg->hdr.payload_len);
 }
