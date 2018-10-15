b744a402a9d9af458b8d2f66fa60c12b56f1159b
Claudio Takahasi <ckt@cesar.org.br>
2018-10-15 06:29:10 -0300

    samples: knot: Add selecting storage automatically
    This patch adds automatic storage source file selection based on the
    selected target platform(BOARD). Supported targets: qemu_x86 and
    nrf52840_pca10056.
    
    Signed-off-by: Claudio Takahasi <ckt@cesar.org.br>

--- core/src/storage.c
+++ core/src/storage.c
@@ -23,82 +23,8 @@
 #define NET_SYS_LOG_LEVEL SYS_LOG_LEVEL_DEBUG
 #define NET_LOG_ENABLED 1
 
-#define ID_HEX_STR_LEN 16 // in bytes
-
-/* Lens */
-
-static u64_t id_val;
-static u8_t uuid_buf[KNOT_PROTOCOL_UUID_LEN];
-static u8_t token_buf[KNOT_PROTOCOL_TOKEN_LEN];
-
-int8_t storage_init(void)
-{
-	NET_DBG("Initializing mock storage");
-
-#if 1
-	/* New device testing */
-	memset(&id_val, 0, sizeof(id_val));
-	memset(uuid_buf, 0, sizeof(uuid_buf));
-	memset(token_buf, 0, sizeof(token_buf));
-#else
-	/* Set known credentials */
-	id_val = 0xDEADBEEFFEEDBABE;
-	strcpy(uuid_buf, "7599eaee-e006-455e-87c1-f9650be8c7cf");
-	strcpy(token_buf, "d2422c409fb5bfcf011292449d6638b480f643d8");
+#if CONFIG_BOARD_NRF52840_PCA10056
+	#include "storage.nrf52840"
+#elif CONFIG_BOARD_QEMU_X86
+	#include "storage.qemu"
 #endif
-
-	return 0;
-}
-
-int8_t storage_reset(void)
-{
-	NET_DBG("Reseting mock storage");
-
-	memset(&id_val, 0, sizeof(id_val));
-	memset(uuid_buf, 0, sizeof(uuid_buf));
-	memset(token_buf, 0, sizeof(token_buf));
-
-	return 0;
-}
-
-int8_t storage_get(enum storage_key key, u8_t *value)
-{
-	switch (key) {
-	case STORAGE_KEY_UUID:
-		if (!strlen(uuid_buf))
-			goto err;
-		memcpy(value, uuid_buf, KNOT_PROTOCOL_UUID_LEN);
-		break;
-	case STORAGE_KEY_TOKEN:
-		if (!strlen(token_buf))
-			goto err;
-		memcpy(value, token_buf, KNOT_PROTOCOL_TOKEN_LEN);
-		break;
-	case STORAGE_KEY_ID:
-		memcpy(value, &id_val, sizeof(id_val));
-		break;
-	}
-
-	return 0;
-
-err:
-	return -ENOENT;
-}
-
-int8_t storage_set(enum storage_key key, const u8_t *value)
-{
-
-	switch (key) {
-	case STORAGE_KEY_UUID:
-		memcpy(uuid_buf, value, KNOT_PROTOCOL_UUID_LEN);
-		break;
-	case STORAGE_KEY_TOKEN:
-		memcpy(token_buf, value, KNOT_PROTOCOL_TOKEN_LEN);
-		break;
-	case STORAGE_KEY_ID:
-		memcpy(&id_val, value, sizeof(id_val));
-		break;
-	}
-
-	return 0;
-}
