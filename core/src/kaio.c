vitbaq/zephyr

samples: knot: Refresh the kaio value
This patch provide a function to refresh the kaio value for a new one.

Signed-off-by: Vitor Barros <vba@cesar.org.br>

samples/net/knot/src/kaio.c
@@ -180,3 +180,17 @@ s8_t kaio_write(u8_t id, knot_value_type *value)
 
 	return 0;
 }
+
+s8_t kaio_refresh_value(u8_t id)
+{
+	struct aio *io;
+
+	if (aio[id].id == 0xff)
+		return -EINVAL;
+
+	io = &aio[id];
+
+	io->refresh = true;
+
+	return 0;
+}

samples/net/knot/src/kaio.h
@@ -15,3 +15,5 @@ u8_t kaio_get_last_id(void);
 s8_t kaio_read(u8_t id, knot_value_type *value);
 
 s8_t kaio_write(u8_t id, knot_value_type *value);
+
+s8_t kaio_refresh_value(u8_t id);

CESARBR/zephyr-knot-sdk

core/src/kaio.c
@@ -161,3 +161,17 @@ s8_t kaio_write(u8_t id, knot_value_type *value)
 
 	return io->write_cb(id);
 }
+
+s8_t kaio_force_send(u8_t id)
+{
+	struct aio *io;
+
+	if (aio[id].id == 0xff)
+		return -EINVAL;
+
+	io = &aio[id];
+
+	io->refresh = true;
+
+	return 0;
+}

core/src/kaio.h
@@ -15,3 +15,5 @@ u8_t kaio_get_last_id(void);
 s8_t kaio_read(u8_t id, knot_value_type *value);
 
 s8_t kaio_write(u8_t id, knot_value_type *value);
+
+s8_t kaio_force_send(u8_t id);