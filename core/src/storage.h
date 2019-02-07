318ab1ffce65dfa99b2fb1bf87b38a7976f61612
Joao Cordeiro <jvcc@cesar.org.br>
2019-02-07 18:41:06 -0300

    core: storage: Remove header functions description
    Remove comments not compliant with project's coding style.
    
    Signed-off-by: Joao Cordeiro <jvcc@cesar.org.br>

--- core/src/storage.h
+++ core/src/storage.h
@@ -14,47 +14,10 @@ struct storage_app_settings {
 	char token[KNOT_PROTOCOL_TOKEN_LEN];
 };
 
-/**
- * @brief Prepare environment to use flash memory.
- *
- * @details Initialise flash device and load values.
- *
- * @retval 0 Success
- * @retval -ERRNO errno code if error
- */
 int8_t storage_init(void);
 
-/**
- * @brief Clear all stored values.
- *
- * @details Clear info of all credentials.
- *
- * @retval 0 Success
- * @retval -ERRNO errno code if error
- */
 int8_t storage_reset(void);
-
-/**
- * @brief Gets value loaded from storage.
- *
- * @details Gets UUID, TOKEN and Device Id loaded from flash memory.
- *
- * @param dest Destination buffer to receive loaded value.
- *
- * @retval 0 for SUCCESS.
- * @retval -ERRNO errno code for Error.
- */
 
 int8_t storage_get(struct storage_app_settings *dest);
 
-/**
- * @brief Store value on NVM.
- *
- * @details Save UUID, TOKEN and Device Id on flash memory.
- *
- * @param src Buffer structure containing values to be stored.
- *
- * @retval 0 for SUCCESS.
- * @retval -ERRNO errno code for Error.
- */
 int8_t storage_set(const struct storage_app_settings *src);
