705bce40f4d73d9d4d390f0b3532ef8726ba8184
Joao Cordeiro <jvcc@cesar.org.br>
2019-02-14 10:36:40 -0300

    core: Start Main application at flash slot 0.1
    Add DTC overlays to core so the Main application is started at
    flash slot 0.1.
    
    Include NRF bootloader files to Setup compilation and call then if
    a bootload is required.
    
    Signed-off-by: Joao Cordeiro <jvcc@cesar.org.br>

--- setup/src/bootloader.c
+++ setup/src/bootloader.c
@@ -9,10 +9,13 @@
 #include <zephyr.h>
 #include <logging/log.h>
 
+#include "third_party/nrf_bootloader_app_start.h"
+
 LOG_MODULE_REGISTER(bootloader, LOG_LEVEL_DBG);
 
 int bootloader_start_main(void)
 {
 	LOG_INF("Starting Main application");
+	nrf_bootloader_app_start();
 	return -1;	/* Return error as not started main application */
 }
