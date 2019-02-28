6c3df5155e2b128552436cbe80b891856314c41e
Joao Cordeiro <jvcc@cesar.org.br>
2019-02-28 07:36:49 -0300

    scripts: cli: Check if $KNOT_BASE is set
    Create module knot_sdk to handle specific operations related to
    the knot-zephyr-sdk.
    
    Create Common class responsible for getting the base path
    for the knot-zephyr-sdk.
    
    Raise an error if the environment variable is not set.
    
    Signed-off-by: Joao Cordeiro <jvcc@cesar.org.br>

--- scripts/cli.py
+++ scripts/cli.py
@@ -4,7 +4,44 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+from os import environ
+
+import sys
 import click
+
+
+"""
+ Singleton class to garrantee that a single instance will be used for
+ its inhereted classes
+"""
+class Singleton(type):
+    __instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls.__instances:
+            cls.__instances[cls] = super(Singleton,
+                                         cls).__call__(*args, **kwargs)
+        return cls.__instances[cls]
+
+
+"""
+ Specific operations related to the KNoT Zephyr SDK
+"""
+class KnotSDK(metaclass=Singleton):
+    knot_path = "" # Common directory and files used by images
+
+    class Constants:
+        KNOT_BASE_VAR = "KNOT_BASE"
+
+    def __init__(self):
+        self.check_env()
+
+    def check_env(self):
+        if self.Constants.KNOT_BASE_VAR not in environ:
+            print('KNoT base path not found.')
+            print('Run: $ source <knot-zephyr-sdk-path>/knot-env.sh')
+            raise KeyError('"$' + self.Constants.KNOT_BASE_VAR +'" not found')
+        self.knot_path = environ[self.Constants.KNOT_BASE_VAR]
+        print('Using KNoT base path: ' + self.knot_path)
 
 
 """
@@ -12,7 +49,10 @@ import click
 """
 @click.group()
 def cli():
-    pass
+    try:
+        KnotSDK()
+    except KeyError as err:
+        sys.exit(err)
 
 @cli.group(help='Build setup and main apps', invoke_without_command=True)
 @click.pass_context
