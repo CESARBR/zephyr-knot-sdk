93f7a3aff991a5863478d9741587e932f5d64772
Allysson Lukas <alla@cesar.org.br>
2019-03-18 18:14:37 -0300

    apps: thermo: Register a new log module
    Register the thermo log module to differentiate app logs from KNoT
    core logs.
    
    Signed-off-by: Allysson Lukas <alla@cesar.org.br>

--- apps/thermo/src/thermo.c
+++ apps/thermo/src/thermo.c
@@ -20,7 +20,7 @@
 #include <knot/knot_types.h>
 #include <knot/knot_protocol.h>
 
-LOG_MODULE_DECLARE(knot, LOG_LEVEL_DBG);
+LOG_MODULE_REGISTER(thermo, LOG_LEVEL_DBG);
 
 /* Tracked value */
 static int thermo = 0;
